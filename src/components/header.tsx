5c2e4b4824d9f62b3b596f6c06052349c71d48b8
Ryuichiro Nagasaka <r.ngsk.todo@gmail.com>
2025-07-21 02:51:29 +0900

    Add MoveLeft Button.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,22 +2,37 @@
 
 import { GrpcRepoContext } from "@/interfaces/react/contexts/grpcRepoContext";
 import { useGrpcConnect } from "@/interfaces/react/hooks/useGrpcConnect";
-import { Home, Settings } from "lucide-react";
+import { Home, MoveLeft, Settings } from "lucide-react";
 import { useContext, useEffect } from "react";
 import { Card, CardContent } from "./ui/card";
+import { usePathname, useRouter } from "next/navigation";
 
 export function Header() {
   const grpcRepo = useContext(GrpcRepoContext);
   const { status, connect } = useGrpcConnect(grpcRepo);
 
+  const pathname = usePathname();
+  const router = useRouter();
+
   useEffect(() => {
     connect();
   }, [connect]);
 
+  const showBackButton = pathname !== "/";
+
   return (
     <header className="flex flex-row items-center justify-between p-4">
+      <div className="flex items-center">
+        {showBackButton && (
+          <button
+            onClick={() => router.back()}
+            className="mr-4 p-1 rounded cursor-pointer"
+          >
+            <MoveLeft className="h-6 w-6" />
+          </button>
+        )}
+      </div>
       <div className="grow">
-        {/* <h1 className="text-2xl font-bold">roomtemp</h1> */}
       </div>
       <Card className="w-64 h-6 mx-6">
         <CardContent className="flex items-center justify-center h-full">
