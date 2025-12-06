637416646e7830672931023592c99178d879bf68
Ryuichiro Nagasaka <r.ngsk.todo@gmail.com>
2025-12-07 02:29:54 +0900

    Delete duplicated mod definitions.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod app_state;
-mod controller;
-mod domain;
-mod infrastructure;
-mod presentation;
-mod repository;
-mod usecase;
-
 fn main() {
     app_lib::run();
 }
